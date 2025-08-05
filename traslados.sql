085793c68efe67c20218c742e5502399f2d965b3
Marco Lanza <mlanzamolina@gmail.com>
2025-08-05 16:40:41 -0600

    psuging

--- traslados.sql
+++ traslados.sql
@@ -3,7 +3,6 @@ SELECT
   s.rnp_seq_number     AS rnp_seq_numb,
   s.dni_solicitante    AS DNI,
   s.nombre_solicitante AS Nombre,
-  -- remove both “a las” and stray “ho” in one regex pass, then trim whitespace
   TRIM(
     REGEXP_REPLACE(s.fecha_solicitud, '(a las|ho)', '')
   )                    AS Fecha_solicitud,
@@ -12,10 +11,13 @@ SELECT
   p.nombre             AS Pais_Destino,
   c.nombre             AS Consulado_Destino,
   s.rnp_lote           AS Lote,
-  s.seq_numb           AS Sobre,
   s.tel                AS Telefono
 FROM solicitudes_traslado_dni_internacional AS s
-JOIN paises                  AS p ON p.id = s.pais_destino
-JOIN consulados_paises       AS c ON c.id = s.ciudad_destino
+JOIN paises            AS p ON p.id = s.pais_destino
+JOIN consulados_paises AS c ON c.id = s.ciudad_destino
 WHERE s.id >= 1145
-ORDER BY s.id ASC;
+  AND STR_TO_DATE(
+        TRIM(REGEXP_REPLACE(s.fecha_solicitud, '(a las|ho)', '')),
+        '%c/%e/%Y %r'
+      ) > STR_TO_DATE('1/3/2023 11:17:13 AM','%c/%e/%Y %r')
+ORDER BY Fecha_solicitud DESC;
